6fcd1ab2647cdb3a3b491f8fb3a6c7c9602257cd
Rodrigo Andrade <rodrigo_andrade@rodrigos-macbook-pro.local>
2018-10-25 13:03:09 -0500

    Added Optional Features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,18 +11,63 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var card: UIView!
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
 
+    @IBOutlet weak var btnOptionOne: UIButton!
+    
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        card.layer.cornerRadius = 20.0
+        
+        frontLabel.layer.cornerRadius=20.0
+        frontLabel.clipsToBounds = true
+        backLabel.layer.cornerRadius=20.0
+        backLabel.clipsToBounds = true
+        
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.4712838093, green: 0.7465088433, blue: 1, alpha: 1)
+        
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.4712838093, green: 0.7465088433, blue: 1, alpha: 1)
+        
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.4712838093, green: 0.7465088433, blue: 1, alpha: 1)
+        
+        
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if frontLabel.isHidden == true {
+            frontLabel.isHidden = false }
+        
     }
+    
+    
+    @IBAction func didTapOptionOne(_ sender: UITapGestureRecognizer) {
+        frontLabel.isHidden = false}
+    
+    @IBAction func didTapOptionTwo(_ sender: UITapGestureRecognizer) {
+        frontLabel.isHidden = true}
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+    frontLabel.isHidden = false}
     
 }
 
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ Simple Flashcards app
 
 ### App Walk-though
 
-<img src="http://g.recordit.co/GrAtAM1x5B.gif" width=200><br>
+<img src="http://recordit.co/4ooKeCSvLd" width=200><br>
 
 ## Required
 - [X] Create New Project in Xcode
@@ -14,6 +14,6 @@ Simple Flashcards app
 - [X] Build in logic to show the answer side when the card is tapped
 - [X] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [X] Toggle the flashcard between the question side and the answer side
+- [X] Style the question and answer side of the card to better distinguish between the two sides
+- [X] Add selectable multiple choice answers beneath the card
